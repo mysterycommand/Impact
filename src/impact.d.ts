746388d055bfb74e1f608b48960627164fa1d222
Matt Hayes <matt@mysterycommand.com>
2019-08-05 12:16:31 -0500

    docs: add note about png modules being strings

--- src/impact.d.ts
+++ src/impact.d.ts
@@ -1,3 +1,6 @@
+/**
+ * @see: https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/lib/react-app.d.ts#L32-L35
+ */
 declare module '*.png' {
   const src: string;
   export default src;
