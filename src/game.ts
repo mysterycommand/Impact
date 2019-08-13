83041d54cefcc6357abcfe26b19da440538ff123
Matt Hayes <matt@mysterycommand.com>
2019-08-13 16:52:12 -0500

    refactor: mvp system

--- src/game.ts
+++ src/game.ts
@@ -1,22 +1,25 @@
-import './game.css';
 import { useFont, Align } from './lib/font';
 
 import path from './media/04b03.font.png';
-import { getEl } from './lib/util';
 
-const canvas = getEl('#canvas') as HTMLCanvasElement;
-const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+import './game.css';
+import { useSystem } from './lib/system';
+
+const {
+  canvas: { width, height },
+  context,
+} = useSystem('#canvas');
 
 (async () => {
   const print = await useFont(path);
   print(
-    ctx,
+    context,
     `\
 It works!
 Multiline too!
 `,
-    canvas.width / 2,
-    canvas.height / 2,
+    width / 2,
+    height / 2,
     Align.Center,
   );
 })();
--- src/lib/system.ts
+++ src/lib/system.ts
@@ -1,3 +1,32 @@
-type SystemOptions = {};
+import { getEl } from './util';
 
-export function useSystem(canvasSelector: string, options?: SystemOptions) {}
+type SystemOptions = {
+  fps: number;
+  width: number;
+  height: number;
+  scale: number;
+};
+
+const defaultOptions: SystemOptions = {
+  fps: 30,
+  width: 320,
+  height: 240,
+  scale: 1,
+};
+
+export function useSystem(
+  canvasSelector: string,
+  options?: Partial<SystemOptions>,
+) {
+  const { fps, width, height, scale } = { ...options, ...defaultOptions };
+
+  const canvas = getEl(canvasSelector) as HTMLCanvasElement;
+  const realWidth = width * scale;
+  const realHeight = height * scale;
+  canvas.width = realWidth;
+  canvas.height = realHeight;
+
+  const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+  return { fps, canvas, context };
+}
