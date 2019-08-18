8db7f07d15936e439341a920785a8ba2bdceded4
Matt Hayes <matt@mysterycommand.com>
2019-08-18 14:18:30 -0400

    chore: rename for clarity

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
 import { useFont, Align } from './lib/font';
-
-import path from './media/04b03.font.png';
-
-import './game.css';
 import { useSystem } from './lib/system';
 
-const {
-  canvas: { width, height },
-  context,
-} = useSystem('#canvas', {
+import path from './media/04b03.font.png';
+import './main.css';
+
+const { scale, canvas, context } = useSystem('#canvas', {
   fps: 60,
   scale: 2,
 });
+
+const { width, height } = canvas;
 
 (async () => {
   const print = await useFont(path);
@@ -20,10 +18,11 @@ const {
     context,
     `\
 It works!
-Multiline too!
+Multiline too!\
 `,
     width / 2,
     height / 2,
     Align.Center,
+    scale,
   );
 })();
