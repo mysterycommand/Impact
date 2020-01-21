0927a3da1f848792b6a7a530ae65c79769f21568
Matt Hayes <matt@mysterycommand.com>
2020-01-21 00:05:07 -0500

    feat: a math util module

---
+++ src/lib/math.ts
@@ -0,0 +1,56 @@
+export const {
+  abs,
+  atan2,
+  ceil,
+  cos,
+  floor,
+  hypot,
+  max,
+  min,
+  PI: π,
+  round,
+  sin,
+  sqrt,
+} = Math;
+
+export function toRadians(degrees: number) {
+  return (degrees / 180) * π;
+}
+
+export function toDegrees(radians: number) {
+  return (radians * 180) / π;
+}
+
+export function isBetween(
+  lowerBound: number,
+  upperBound: number,
+  value: number,
+  lowerBoundInclusive = true,
+  upperBoundInclusive = false,
+) {
+  if (upperBound < lowerBound) {
+    [lowerBound, upperBound] = [upperBound, lowerBound];
+  }
+
+  return (
+    (lowerBoundInclusive ? lowerBound <= value : lowerBound < value) &&
+    (upperBoundInclusive ? value <= upperBound : value < upperBound)
+  );
+}
+
+export function isOutside(
+  lowerBound: number,
+  upperBound: number,
+  value: number,
+  lowerBoundInclusive = false,
+  upperBoundInclusive = true,
+) {
+  if (upperBound < lowerBound) {
+    [lowerBound, upperBound] = [upperBound, lowerBound];
+  }
+
+  return (
+    (lowerBoundInclusive ? lowerBound <= value : lowerBound < value) &&
+    (upperBoundInclusive ? value <= upperBound : value < upperBound)
+  );
+}
