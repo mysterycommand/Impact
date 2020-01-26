2a8041b917beb9233f4c796f43b62ee85edf16e4
Matt Hayes <matt@mysterycommand.com>
2020-01-26 12:27:08 -0500

    feat: basic implementation, still need to flesh out the actual game play functionality

--- src/games/jumpnrun/entities/coin.ts
+++ src/games/jumpnrun/entities/coin.ts
@@ -1,3 +1,28 @@
 import Entity from '../../../lib/entity';
+import SpriteSheet from '../../../lib/sprite-sheet';
 
-export default class Coin extends Entity {}
+import coinPath from '../media/coin.png';
+
+import Player from './player';
+
+export default class Coin extends Entity {
+  public size = { x: 36, y: 36 };
+  public spriteSheet = new SpriteSheet(coinPath, 36, 36);
+
+  constructor(x: number, y: number) {
+    super(x, y);
+    this.addAnim('idle', 0.1, [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2]);
+  }
+
+  public update() {
+    this.currAnim?.update();
+  }
+
+  public check(other: Entity) {
+    if (other instanceof Player) {
+      // other.giveCoins(1);
+      // this.sfxCollect.play();
+      // this.kill();
+    }
+  }
+}
