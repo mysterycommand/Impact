ebdc6bff0d644b7f778cdb373ae852735102ca57
Matt Hayes <matt@mysterycommand.com>
2020-02-02 11:08:26 -0500

    feat: new fonts, new title screen

--- src/games/tornaire/index.ts
+++ src/games/tornaire/index.ts
@@ -4,8 +4,12 @@ import { system, input } from '../../lib/impact';
 import KeyCode from '../../lib/key-code';
 import Music from '../../lib/music';
 
-import fontPath from './media/fredoka-one.font.png';
-const font = new Font(fontPath);
+// import fontPath from './media/fonts/fredoka-one.font.png';
+import titleFontPath from './media/fonts/semplicità-ombra-extra-large.png';
+const titleFont = new Font(titleFontPath);
+
+import bodyFontPath from './media/fonts/semplicità-medium.png';
+const bodyFont = new Font(bodyFontPath);
 
 import musicPath from './media/music/Tornaire OST - Overworld BGM LOOP - Diwa de Leon.mp3';
 
@@ -14,7 +18,8 @@ import { once } from '../../lib/util';
 
 export default class Tornaire extends Game {
   public readonly gravity = 800;
-  public font = font;
+  public titleFont = titleFont;
+  public bodyFont = bodyFont;
   protected clearColor = '#e7e7e7';
   protected cellSize = 70;
 
@@ -48,13 +53,20 @@ export default class Tornaire extends Game {
   public draw() {
     super.draw();
 
-    const scale = 3;
-
-    system.context.save();
-    system.context.scale(scale, scale);
-    const hw = system.width / (2 * scale);
-    const hh = system.height / (2 * scale) - (this.font.height * scale) / 2;
-    this.font.print('tornaire', hw, hh, Align.Center);
-    system.context.restore();
+    const hw = system.width / 2;
+    const hh = system.height / 2 - this.titleFont.height / 2;
+    this.titleFont.print('tornaire', hw, hh, Align.Center);
+    this.bodyFont.print(
+      'WASD TO MOVE, L TO JUMP',
+      hw,
+      hh + this.titleFont.height / 2 + this.bodyFont.height * 3,
+      Align.Center,
+    );
+    this.bodyFont.print(
+      'CLICK ANYWHERE TO START',
+      hw,
+      hh + this.titleFont.height / 2 + this.bodyFont.height * 4,
+      Align.Center,
+    );
   }
 }
